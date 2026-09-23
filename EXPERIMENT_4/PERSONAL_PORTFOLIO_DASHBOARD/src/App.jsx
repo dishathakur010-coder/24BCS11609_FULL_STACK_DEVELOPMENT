662cb74ad143cb9cdf7041003fc1054b87972a3f
dishathakur010-coder <dishathakur010@gmail.com>
2026-09-23 11:05:06 +0530

    Create main App component with routing setup

---
+++ EXPERIMENT_4/PERSONAL_PORTFOLIO_DASHBOARD/src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="profile" element={<Profile />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
